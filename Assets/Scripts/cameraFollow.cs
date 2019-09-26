e1891532ef667dd89c51d662c9bff6103ebcd76d
PandaCuddles <23082189+PandaCuddles@users.noreply.github.com>
2019-09-25 22:41:58 -0500

    Test Environment v0.0.1 Initial Commit

---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+
+
+  // Update is called once per frame
+  void FixedUpdate () {
+    // just referencing trasnform.<item> references the current object the script is attached to
+    Vector3 temp = new Vector3(transform.position.x,transform.position.y,Camera.main.transform.position.z);
+    Camera.main.transform.position = temp;
+
+    }
+}
