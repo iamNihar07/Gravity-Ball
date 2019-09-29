db3c3a18e2331ae1fed5a25f70fb4378af653fe4
PandaCuddles <23082189+PandaCuddles@users.noreply.github.com>
2019-09-29 15:52:58 -0500

    Added UnityFiles for unpackaged unity assets.

---
+++ UnityFiles/GravityBounce/Assets/Scripts/playerHealing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealing : MonoBehaviour
+{
+
+    GameObject temp;
+    PlayerHealth health;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      if (collision.gameObject.tag == "Player")
+      {
+        temp = GameObject.FindWithTag("Player");
+        health = (PlayerHealth) temp.GetComponent(typeof(PlayerHealth));
+        health.raiseHealth();
+      }
+
+    }
+}
