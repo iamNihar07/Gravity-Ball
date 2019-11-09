617021def25e63aabdaa701bf11fd3f2ca087c06
PandaCuddles <23082189+PandaCuddles@users.noreply.github.com>
2019-11-09 16:44:27 -0600

    Deleted oldest project folders. Added volume slider. Added colliders for level 3.

---
+++ UnityFiles/GravityBounce_v0.6.1/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    private GameObject _temp;
+    private PlayerHealth _health;
+    private Text _healthText;
+
+    private void Update()
+    {
+      // Gets the UI component to diplay player health
+      _healthText = this.GetComponent(typeof(Text)) as Text;
+
+      // Player object has a script with the player's health
+      _temp = GameObject.FindWithTag("Player");
+      _health = _temp.GetComponent(typeof(PlayerHealth)) as PlayerHealth;
+
+      // Update the UI display with the player's current health
+      // If statement is to make sure neither component returned null
+      if (_healthText != null && _health != null)
+      {
+          _healthText.text = "Health : " + _health.GetHealth();
+      }
+
+      // Display game over screen if player's heath reaches zero'
+      if(_health != null &&_health.GetHealth() <= 0)
+      {
+          _temp.GetComponent<FailedLevel>().healthDroppedToZero();
+      }
+
+    }
+}
---
+++ UnityFiles/GravityBounce_v0.6.1/Assets/Scripts/ObjectPooling/WallShift.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallShift : MonoBehaviour
+{
+    private Collider2D collider;
+
+    Vector3 m_Size;
+
+    Vector3 shift;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the object size and store it (returns vector with size
+        // Possible use this for the actual image, rather than the collider
+        collider = transform.GetComponent<Collider2D>();
+        m_Size = collider.bounds.size;
+        
+        // Prints out size vector
+        Debug.Log("Collider Size : " + m_Size);
+        
+        // shifts the wall object above or below the previous object 
+        shift.x = collider.transform.position.x;
+        shift.y = collider.transform.position.y - m_Size.y * 2;
+        shift.z = collider.transform.position.z;
+        
+        // This changes the position of the object to the new position
+        transform.position = shift;
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
